6ca7d0800512efc5c860244cbd7579b9d4ebd305
n <e>
2018-10-02 13:12:23 +0300

    db file added.

--- resources/views/templates/country.blade.php
+++ resources/views/templates/country.blade.php
@@ -135,7 +135,10 @@ Google.{{$obj->country_iso_code}}
 
             <div role="tabpanel" class="tab-pane" id="news">
                 News 
-				<div class="empty"></div>
+				<div class="empty">
+				<iframe src="https://www.google.by/search?ei=FP-xW-GjNrKPmgX2wab4Dg&q={{$obj->country_name}}+news&oq=ru&gs_l=psy-ab.3.1.35i39k1l2j0i67k1j0i131i67k1j0i67k1j0i131i67k1j0i67k1l2j0i131k1l2.55564.55795.0.58119.2.2.0.0.0.0.268.268.2-1.1.0....0...1c.1.64.psy-ab..1.1.267....0.U0B-xTXu9mQ">
+</iframe>
+				</div>
             </div>
             <div role="tabpanel" class="tab-pane" id="messages">
                 Weathers
